3fe92eb42284c24f8e13694615529eb90575ef83
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-30 17:25:25 -0300

    feat: structuring and applying the stylizations

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { Container, Header } from "./styles";
+import {
+  Container,
+  Header,
+  Photo,
+  User,
+  UserGreeting,
+  UserInfo,
+  UserName,
+  UserWrapper,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Header>
-        <Text>Cleyton</Text>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: "https://github.com/Cleyton-1995.png" }} />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Cleyton</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
       </Header>
     </Container>
   );
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,11 +1,41 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-   flex: 1;
-   background-color: ${({theme}) => theme.colors.background};
-   `; 
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
 
 export const Header = styled.View`
-   width: 100%;
-   background-color: ${({theme}) => theme.colors.primmary};
-`
+  width: 100%;
+  height: 278px;
+  background-color: ${({ theme }) => theme.colors.primmary};
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+`;
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+export const Photo = styled.Image`
+  width: 48px;
+  height: 48px;
+  border-radius: 10px;
+`;
+export const User = styled.View`
+  margin-left: 17px;
+`;
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;

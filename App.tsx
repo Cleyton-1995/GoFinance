c5b8accf7f8d02f69f3ba123238841463c08d939
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-30 15:39:51 -0300

    feat: Wrapping the App with Themes

--- App.tsx
+++ App.tsx
@@ -2,21 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Dashboard } from './src/screens/Dashboard';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ThemeProvider theme={theme}>
       <Dashboard title="Boas Vindas"/>
       <StatusBar style="auto" />
-    </View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

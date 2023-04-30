5e6cf2478787d85c71e398bdc88e68c3e03f1633
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-30 16:12:46 -0300

    feat: Configuring the fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Dashboard } from './src/screens/Dashboard';
-import { ThemeProvider } from 'styled-components';
-import theme from './src/global/styles/theme';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Dashboard } from "./src/screens/Dashboard";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/global/styles/theme";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard title="Boas Vindas"/>
+      <Dashboard title="Boas Vindas" />
       <StatusBar style="auto" />
     </ThemeProvider>
   );

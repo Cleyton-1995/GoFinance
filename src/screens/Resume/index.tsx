53f7707a41beccc17de58c63992fe511e9b1e853
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-05-09 15:32:48 -0300

    feat: accessing AsyncStorage, taking transactions and summing them up by category

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import { Container, Header, Title } from './styles';
-import { HistoryCard } from '../../Components/HistoryCard';
+import React, { useEffect, useState } from "react";
+import { Container, Header, Title } from "./styles";
+import { HistoryCard } from "../../Components/HistoryCard";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { categories } from "../../utils/categories";
+
+interface TransactionData {
+  type: "positive" | "negative";
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+}
+
+interface CategoryData {
+  name: string;
+  total: string;
+}
 
 export function Resume() {
+   const [ totalByCategories, setTotalByCategories] = useState<CategoryData[]>([])
+
+  async function loadData() {
+    const dataKey = "@gofinances:transactions";
+    const reponse = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = reponse ? JSON.parse(reponse) : [];
+
+    const expensives = responseFormatted.filter(
+      (expensive: TransactionData) => expensive.type === "negative"
+    );
+
+    const totalByCategory: CategoryData[] = [];
+
+    categories.forEach((category) => {
+      let categorySum = 0;
+
+      expensives.forEach((expensive: TransactionData) => {
+        if (expensive.category === category.key) {
+          categorySum += Number(expensive.amount);
+        }
+      });
+
+      if (categorySum > 0) {
+        const total = categorySum.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        });
+        totalByCategory.push({
+          name: category.name,
+          total,
+        });
+      }
+    });
+
+    setTotalByCategories(totalByCategory);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <Container>
       <Header>
-         <Title>Resumo por categoria</Title>
+        <Title>Resumo por categoria</Title>
       </Header>
 
-      <HistoryCard 
-         title="Compras"
-         amount="R$ 150,50"
-         color="red"
-      />
-      <HistoryCard 
-         title="Compras"
-         amount="R$ 150,50"
-         color="blue"
-      />
+      <HistoryCard title="Compras" amount="R$ 150,50" color="red" />
     </Container>
   );
 }

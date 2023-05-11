04a01c778e71b315aeaa702b97d841c70cc48ca1
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-05-11 16:09:56 -0300

    feat: Creating authentication interface

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import { Container } from './styles';
+import React from "react";
+import { Container, Footer, Header, SignInTitle, Title, TitleWrapper } from "./styles";
+
+import AppleSvg from "../../assets/apple.svg";
+import GoogleSvg from "../../assets/google.svg";
+import LogoSvg from "../../assets/logo.svg";
 
 export function SignIn() {
   return (
     <Container>
+      <Header>
+        <TitleWrapper>
+          <LogoSvg 
+           width="120" 
+           height="68"
+          />
 
+          <Title>
+            Controle suas {"\n"}
+            finanças de forma {"\n"}
+            muito simples
+          </Title>
+        </TitleWrapper>
+
+        <SignInTitle>
+         Faça seu login com {"\n"}
+         uma das contas abaixo
+        </SignInTitle>
+      </Header>
+
+      <Footer>
+
+      </Footer>
     </Container>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,5 +1,35 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-
+   flex: 1;
+`;
+export const Header = styled.View`
+   width: 100%;
+   height: 70%;
+   background-color: ${({theme}) => theme.colors.primmary};
+   justify-content: flex-end;
+   align-items: center;
+`;
+export const TitleWrapper = styled.View`
+   align-items: center;
+`;
+export const Title = styled.Text`
+   font-family: ${({theme}) => theme.fonts.medium};
+   color: ${({theme}) => theme.colors.shape};
+   font-size: 30px;
+   text-align: center;
+   margin-top: 40px;
+`;
+export const SignInTitle = styled.Text`
+   font-family: ${({theme}) => theme.fonts.regular};
+   color: ${({theme}) => theme.colors.shape};
+   font-size: 16px;
+   text-align: center;
+   margin-top: 40px;
+   margin-bottom: 40px;
+`;
+export const Footer = styled.View`
+   width: 100%;
+   height: 30%;
+   background-color: ${({theme}) => theme.colors.secundary};
 `;

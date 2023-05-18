8155a3dba780b4cdd3d1b50a370bb8e501b96f06
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-05-18 15:47:16 -0300

    faet: Authentication with google account successfully performed

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, createContext, useContext } from "react";
+import React, { ReactNode, createContext, useContext, useState } from "react";
 import * as AuthSession from "expo-auth-session";
 
 interface AuthProviderProps {
@@ -17,14 +17,17 @@ interface IAuthContextData {
   signInWithGoogle(): Promise<void>;
 }
 
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
+
 const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const user = {
-    id: "12121212",
-    name: "Cleyton Costa",
-    email: "cleyton@gmail.com",
-  };
+  const [user, setUser] = useState<User>({} as User);
 
   async function signInWithGoogle() {
     try {
@@ -36,19 +39,36 @@ function AuthProvider({ children }: AuthProviderProps) {
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-      const response = await AuthSession.startAsync({ authUrl });
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
 
-      console.log(response);
+      if (type === "success") {
+        const response = await fetch(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
+        );
+
+        const userInfo = await response.json();
+        
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture
+        })
+      }
     } catch (error) {
       throw new Error(error);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ 
-      user, 
-      signInWithGoogle 
-    }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        signInWithGoogle,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );

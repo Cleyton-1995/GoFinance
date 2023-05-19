8d49f52f07585cae94543ae425a406c3173e308a
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-05-19 16:28:17 -0300

    faet: configuring routes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
-import {
-  useFonts,
-  Poppins_400Regular,
-  Poppins_500Medium,
-  Poppins_700Bold,
-} from "@expo-google-fonts/poppins";
 import * as SplashScreen from "expo-splash-screen";
-import { SignIn } from "./src/screens/SignIn";
 import { AuthProvider } from "./src/hooks/auth";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Routes } from "./src/routes";
@@ -17,37 +10,14 @@ import { Routes } from "./src/routes";
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_700Bold,
-  });
-
-  useEffect(() => {
-    async function prepare() {
-      await SplashScreen.preventAutoHideAsync();
-    }
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
-        <StatusBar style="light" translucent backgroundColor="transparent" />
-      </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <AuthProvider>
+            <Routes />
+          </AuthProvider>
+          <StatusBar style="light" translucent backgroundColor="transparent" />
+        </ThemeProvider>
     </GestureHandlerRootView>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,46 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import { AuthRoutes } from './auth.routes';
-import { AppRoutes } from './app.routes';
+import { NavigationContainer } from "@react-navigation/native";
+import React, { useCallback, useEffect } from "react";
+import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
+import { useAuth } from "../hooks/auth";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import * as SplashScreen from "expo-splash-screen";
 
 export function Routes() {
+  const { user } = useAuth();
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  console.log(user);
   return (
-    <NavigationContainer>
-      <AuthRoutes/>
+    <NavigationContainer onReady={onLayoutRootView} >
+      {user.id ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
